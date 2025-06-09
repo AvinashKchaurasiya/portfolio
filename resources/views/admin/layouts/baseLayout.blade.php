94aab7e4f6c850b188b5f8869a9950f009502320
AvinashKmogulas <avinash.mogulas@gmail.com>
2025-06-09 16:57:49 +0530

    path changes

--- resources/views/admin/layouts/baseLayout.blade.php
+++ resources/views/admin/layouts/baseLayout.blade.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title ? $title : 'Company Dashboard | Smart Software Solutions for Smarter Business' }}</title>
-    <link rel="icon" href="{{ URL::asset('Backend/company/assets/images/logo/logo.png') }}">
-    <link rel="stylesheet" href="{{ URL::asset('Backend/company/assets/css/custom.css') }}" />
-    <link rel="stylesheet" href="{{ URL::asset('Backend/company/assets/css/mdb.min.css') }}" />
-    <link rel="stylesheet" href="{{ URL::asset('Backend/company/assets/font/bootstrap-icons.css') }}" />
-    <link rel="stylesheet" href="{{ URL::asset('Backend/company/assets/css/all.min.css') }}" />
-    <link rel="stylesheet" href="{{ URL::asset('Backend/company/assets/css/animate.css') }}" />
-    <link rel="stylesheet" href="{{ URL::asset('Backend/company/assets/js/jquery-3.7.1.min.js') }}" />
-    <link rel="stylesheet" href="{{ URL::asset('Backend/company/assets/css/sweetalert2.min.css') }}" />
+    <link rel="icon" href="{{ URL::asset('admin/images/logo/logo.png') }}">
+    <link rel="stylesheet" href="{{ URL::asset('admin/assets/css/custom.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('admin/assets/css/mdb.min.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('admin/assets/font/bootstrap-icons.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('admin/assets/css/all.min.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('admin/assets/css/animate.css') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('admin/assets/js/jquery-3.7.1.min.js') }}" />
+    <link rel="stylesheet" href="{{ URL::asset('admin/assets/css/sweetalert2.min.css') }}" />
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
     <script>
         document.addEventListener('DOMContentLoaded', function() {
@@ -46,10 +46,10 @@
 
     @yield('content')
 
-    <script src="{{ URL::asset('Backend/company/assets/js/mdb.umd.min.js') }}"></script>
-    <script src="{{ URL::asset('Backend/company/assets/js/mdb.min.js') }}"></script>
-    <script src="{{ URL::asset('Backend/company/assets/js/wow.min.js') }}"></script>
-    <script src="{{ URL::asset('Backend/company/assets/js/SweetAlert2.js') }}"></script>
+    <script src="{{ URL::asset('admin/assets/js/mdb.umd.min.js') }}"></script>
+    <script src="{{ URL::asset('admin/assets/js/mdb.min.js') }}"></script>
+    <script src="{{ URL::asset('admin/assets/js/wow.min.js') }}"></script>
+    <script src="{{ URL::asset('admin/assets/js/SweetAlert2.js') }}"></script>
     <!-- Navbar Scroll Effect -->
     <script>
         new WOW().init();
