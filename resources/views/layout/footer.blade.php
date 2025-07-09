bea4e1505cc4e3ec36cfa4a6b30d8c5917540d16
AvinashKmogulas <avinash.mogulas@gmail.com>
2025-07-09 11:19:01 +0530

    change in  footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,17 +1,11 @@
  <footer id="footer" class="footer position-relative light-background">
 
-     {{-- <div class="container">
-         <div class="copyright text-center ">
-             <p>© <span>Copyright</span> <strong class="px-1 sitename">iPortfolio</strong> <span>All Rights
-                     Reserved</span></p>
+    <div class="container">
+        <div class="copyright text-center ">
+            <p>© <span>Copyright</span> <strong class="px-1 sitename">z1i Innovations</strong> <span>All Rights Reserved</span></p>
+        </div>
+        <div class="credits">Designed and Developed by <a href="https://z1iinnovation.com/" target="_blank">Z1i Innovations</a>
          </div>
-         <div class="credits">
-             <!-- All the links in the footer should remain intact. -->
-             <!-- You can delete the links only if you've purchased the pro version. -->
-             <!-- Licensing information: https://bootstrapmade.com/license/ -->
-             <!-- Purchase the pro version with working PHP/AJAX contact form: [buy-url] -->
-             Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-         </div>
-     </div> --}}
+    </div>
 
  </footer>
