bba1f0ec8fe21bfe9de101b405fa39ceabc5e972
AvinashKmogulas <avinash.mogulas@gmail.com>
2025-07-10 18:30:49 +0530

    add website url on project details page

--- resources/views/project-details.blade.php
+++ resources/views/project-details.blade.php
@@ -12,6 +12,9 @@
                             <div class="col-md-8">
                                 <div class="card-body">
                                     <h5 class="card-title">{{ $project->project_name }} - {{ $project->service->title }}</h5>
+                                    @if($project->url == '')
+                                        <a href="{{ $project->url  }}" style="text-decoration: none;">{{ $project->url }}</a>
+                                    @endif
                                     <hr/>
                                     <p class="card-text">{!! $project->description !!}</p>
                                 </div>
